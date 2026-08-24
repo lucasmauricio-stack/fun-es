dfc8b220b120d33507d544f6f6febb84c015ed0b
lucasmauricio-stack <lucas.mauricio@aluno.ifsp.edu.br>
2026-08-24 20:07:55 -0300

    Add files via upload

---
+++ Baseárea1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static int multiplica(int a, int b)
+        {
+            return a * b;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            int altura;
+            int baseretangulo;
+            int resultado;
+
+            Console.Write("Digite a altura: ");
+            altura = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a base: ");
+            baseretangulo = int.Parse(Console.ReadLine());
+
+            Console.Write("A área do retangulo é: ");
+            resultado = multiplica(altura, baseretangulo);
+            Console.WriteLine(resultado);
+
+
+        }
+    }
+}
