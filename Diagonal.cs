a3b5da016b33ce66012490815a3d0ee5248bcb6a
lucasmauricio-stack <lucas.mauricio@aluno.ifsp.edu.br>
2026-08-24 20:17:18 -0300

    Add files via upload

---
+++ Diagonal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static int áreadiagonal(int a)
+        {
+            return a * a /2;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            int diagonal;
+            int resultado;
+
+            Console.Write("Digite o valor da diagonal: ");
+            diagonal = int.Parse(Console.ReadLine());
+
+            Console.Write("A área do quadrado é: ");
+            resultado = áreadiagonal(diagonal);
+            Console.WriteLine(resultado);
+
+
+        }
+    }
+}
