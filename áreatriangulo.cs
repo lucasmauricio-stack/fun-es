f766dcad66f28eeb7e130a272d81604413eaf6d8
lucasmauricio-stack <lucas.mauricio@aluno.ifsp.edu.br>
2026-08-24 20:24:35 -0300

    Add files via upload

---
+++ áreatriangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static double AreaTriangulo(double baseTriangulo, double altura)
+        {
+            return (baseTriangulo * altura) / 2.0;
+        }
+
+        static void Main(string[] args)
+        {
+            double baseTriangulo;
+            double altura;
+            double resultado;
+
+            Console.Write("Digite o valor da base: ");
+            baseTriangulo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da altura: ");
+            altura = double.Parse(Console.ReadLine());
+            resultado = AreaTriangulo(baseTriangulo, altura);
+
+            Console.WriteLine($"A área do triângulo é: {resultado}");
+        }
+    }
+}
