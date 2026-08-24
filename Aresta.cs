2577f8daa207a59d7574f97212766129078f3434
lucasmauricio-stack <lucas.mauricio@aluno.ifsp.edu.br>
2026-08-24 20:14:01 -0300

    Add files via upload

---
+++ Aresta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static int multiplica(int a)
+        {
+            return a * a;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            int aresta;
+            int resultado;
+
+            Console.Write("Digite o valor da aresta: ");
+            aresta = int.Parse(Console.ReadLine());
+
+            Console.Write("A área do quadrado é: ");
+            resultado = multiplica(aresta);
+            Console.WriteLine(resultado);
+
+
+        }
+    }
+}
