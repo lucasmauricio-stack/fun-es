f65ed1b07a8a43eda2c7062f23ca5fdd58b44e2f
lucasmauricio-stack <lucas.mauricio@aluno.ifsp.edu.br>
2026-08-24 21:00:32 -0300

    Add files via upload

---
+++ Milhakm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static double ConverterMilhasParaKm(double milhas)
+        {
+            return milhas * 1.852;
+        }
+
+        static void Main(string[] args)
+        {
+            double milhas;
+            double resultadoKm;
+
+            Console.Write("Digite o número de Milhas: ");
+            milhas = double.Parse(Console.ReadLine());
+
+            resultadoKm = ConverterMilhasParaKm(milhas);
+
+            Console.WriteLine($"Isso equivale a: {resultadoKm} km");
+        }
+    }
+}
